9d3efba88169e9f05d6c7d52292132f8ac37f1d2
Vaishnavi Gudimella <vgudimel@uwo.ca>
2026-07-21 18:53:45 -0400

    Remove one-off diagnostic scripts (preserved in v1-pilot tag)

--- f1_check.py
+++
@@ -1,6 +0,0 @@
-# f1_check.py
-from memory import retrieve_memory, get_or_create_collection
-
-PID = "persist01"
-print("Collection count on fresh process:", get_or_create_collection(PID).count())
-print("RETRIEVED:", repr(retrieve_memory(PID, "favorite depth")))
